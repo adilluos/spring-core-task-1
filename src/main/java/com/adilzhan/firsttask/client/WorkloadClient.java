be3d24d76b90385a85a21de78bcd62e0a84ab250
Adilzhan <adilzhan1210@gmail.com>
2025-10-30 01:30:42 +0500

    Added service2service JWT between firsttask and workload-service

--- src/main/java/com/adilzhan/firsttask/client/WorkloadClient.java
+++ src/main/java/com/adilzhan/firsttask/client/WorkloadClient.java
@@ -1,11 +1,12 @@
 package com.adilzhan.firsttask.client;
 
+import com.adilzhan.firsttask.config.FeignAuthConfig;
 import com.adilzhan.firsttask.dto.WorkloadUpdateRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "workload-service")  // must match Eureka service name
+@FeignClient(name = "workload-service", configuration = FeignAuthConfig.class)
 public interface WorkloadClient {
 
     @PostMapping("/api/v1/workload/update")
